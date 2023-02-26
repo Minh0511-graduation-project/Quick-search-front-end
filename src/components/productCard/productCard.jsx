9765e7f5941f8be5ba4d1f1e90a7c1ae290165c4
Minh Phạm Vũ <minh.pv@teko.vn>
2023-02-26 23:11:57 +0700

    create cards

---
+++ src/components/productCard/productCard.jsx
@@ -0,0 +1,19 @@
+import {Card, Layout} from "antd";
+import "./productCard.css";
+import Meta from "antd/es/card/Meta";
+
+const productCard = (props) => {
+    return(
+        <Layout className={"product-card"}>
+            <Card
+                hoverable
+                style={{ width: 240 }}
+                cover={<img alt="example" src={props.imageUrl} />}
+            >
+                <Meta title={props.name} description={props.price} />
+            </Card>
+        </Layout>
+    );
+};
+
+export default productCard;
--- src/pages/searchResults/searchResults.jsx
+++ src/pages/searchResults/searchResults.jsx
@@ -1,13 +1,27 @@
 import {Layout, Space} from "antd";
 import "./searchResults.css";
 import SearchHeading from "../../components/searchHeading/searchHeading";
+import ProductCard from "../../components/productCard/productCard";
 
 const SearchResults = () => {
+    const products = [
+        {image_url: "bit.ly/3KAnCcx", name: "Tai nghe 1", price: "100000đ"},
+        {image_url: "bit.ly/3KAnCcx", name: "Tai nghe 2", price: "200000đ"},
+        {image_url: "bit.ly/3KAnCcx", name: "Tai nghe 3", price: "300000đ"},
+        {image_url: "bit.ly/3KAnCcx", name: "Tai nghe 4", price: "400000đ"},
+        {image_url: "bit.ly/3KAnCcx", name: "Tai nghe 5", price: "500000đ"},
+    ]
     return (
         <Layout className={"search-results"}>
             <SearchHeading/>
             <Space>
-                <h1>Search results</h1>
+                {products?.map((product) => (
+                    <ProductCard
+                        imageUrl={product.image_url}
+                        name={product.name}
+                        price={product.price}
+                    />
+                ))}
             </Space>
         </Layout>
     );
